1fb1d75d562835b30bb5d63101c98a222536b7ec
Chris <129601042+Operations-Department@users.noreply.github.com>
2024-04-15 19:27:48 -0500

    update api key to include 3 day forecast data

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const searchbox = document.getElementById('searchbox');
 
 async function getWeather(country) {
     try {
-        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b04164c4e2604ea99d424433241104&q=` + country + `&aqi=no`, {mode: 'cors'});
+        const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=b04164c4e2604ea99d424433241104&q=` + country + `&days=3&aqi=yes&alerts=no
+        `, {mode: 'cors'});
         const data = await response.json();
         console.log(data);
     } catch (error) {
