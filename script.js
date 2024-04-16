164447ca1cb3181c6a05bd3ef1aadae06c8ad099
Chris <129601042+Operations-Department@users.noreply.github.com>
2024-04-15 19:23:46 -0500

    update document title and add searchbox; add function to process user search

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-async function getweather() {
+const searchbox = document.getElementById('searchbox');
+
+async function getWeather(country) {
     try {
-        const response = await fetch('https://api.weatherapi.com/v1/current.json?key=b04164c4e2604ea99d424433241104&q=tashkent&aqi=no', {mode: 'cors'});
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=b04164c4e2604ea99d424433241104&q=` + country + `&aqi=no`, {mode: 'cors'});
         const data = await response.json();
         console.log(data);
     } catch (error) {
@@ -8,4 +10,11 @@ async function getweather() {
     }
 };
 
-window.onload = getweather();
+window.onload = getWeather('tashkent');
+
+searchbox.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
+        let searchedCountry = searchbox.value;
+        getWeather(searchedCountry);
+    }
+});
