660b31645f2554c57434d3d0abbd318c5238601f
Chris <129601042+Operations-Department@users.noreply.github.com>
2024-04-18 19:22:00 -0500

    improve layout; improve  background for mobile; add updateDOM function - put weather data on dom

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const searchbox = document.getElementById('searchbox');
+const area = document.getElementById('area');
+const time = document.getElementById('time');
+const condition = document.getElementById('condition');
+const conditionIconElement = document.getElementById('condition-icon');
+const temp = document.getElementById('temp');
+
 
 async function getWeather(country) {
     try {
@@ -6,16 +12,44 @@ async function getWeather(country) {
         `, {mode: 'cors'});
         const data = await response.json();
         console.log(data);
+        return data;
     } catch (error) {
         console.error('Something terrible happened here', error);
+        return null;
     }
 };
 
-window.onload = getWeather('tashkent');
+function updateDOM(data) {
+    if (data) {
+
+        const city = data.location.name;
+        const region = data.location.region;
+        const country = data.location.country;
+
+        const localtime = data.current.last_updated;
+
+        const conditionText = data.current.condition.text;
+        const conditionIcon = data.current.condition.icon;
+
+        const tempF = data.current.temp_f;
+        const tempC = data.current.temp_c;
+
+        area.textContent = `${city}, ${region}, ${country}`;
+        time.textContent = localtime;
+        condition.textContent = conditionText;
+        conditionIconElement.src = `https:${conditionIcon}`;
+        temp.textContent = `${tempF}°F / ${tempC}°C`;
+
+    } else {
+        console.log('No data available');
+    }
+}
+
+window.onload = getWeather('tashkent').then(updateDOM);
 
 searchbox.addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
         let searchedCountry = searchbox.value;
-        getWeather(searchedCountry);
+        getWeather(searchedCountry).then(updateDOM);
     }
 });
